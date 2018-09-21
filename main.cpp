51a9b58b64c9f8c86cff404b99dc819b487e15e4
Camila Fernandez <camila.fernandez@utec.edu.pe>
2018-09-21 13:08:34 -0500

    Terminado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,13 +8,13 @@ void print(int numbers[], int size);
 void countingSort(int numbers[], int size);
 
 int main(int argc, char *argv[]) {
-    int numbers[] = {1, 4, 1, 2, 7, 5, 2};
+    int numbers[] = {53, 89, 23, 29, 64, 95, 76, 34, 33, 98, 30};
     int size = sizeof(numbers) / sizeof(*numbers);
 
     print(numbers, size);
     countingSort(numbers, size);
     print(numbers, size);
-    
+
     system("read");
     return EXIT_SUCCESS;
 }
@@ -26,5 +27,28 @@ void print(int numbers[], int size) {
 }
 
 void countingSort(int numbers[], int size) {
-    // TODO
+    int min = *min_element(numbers,numbers+size);
+    int csize = *max_element(numbers, numbers+size) -min +1;
+    int count[csize];
+    memset(count, 0, sizeof(count));
+    int sorted[size];
+
+
+    for(int i = 0; numbers[i]; ++i){ //1.
+        ++count[numbers[i]-min];
+    }
+
+    for(int i=1; i<csize+1; i++){
+      count[i]+=count[i-1];
+    }
+
+    for(int i=0; numbers[i]; ++i){
+      sorted[count[numbers[i]-min]-=1] = numbers[i];
+    }
+
+    for(int i=0; numbers[i]; i++){
+      numbers[i]=sorted[i];
+    }
+
+    cout << endl;
 }
